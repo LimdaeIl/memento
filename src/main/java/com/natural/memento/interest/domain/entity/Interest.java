2273d45f16059ead92baaf8a445b89891dd77f13
LimdaeIl <optional94@naver.com>
2026-01-18 22:45:17 +0900

    ✨#9: 관심사 엔티티 설계
    큰 범주로 관심사 상태 기반을 작성하고, 큰 범주 안에서 구체적인 관심사를 관리하도록 합니다.

---
+++ src/main/java/com/natural/memento/interest/domain/entity/Interest.java
@@ -0,0 +1,42 @@
+package com.natural.memento.interest.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "v1_interests")
+@Entity
+public class Interest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "interest_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InterestType interestType;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    private Interest(InterestType interestType, String name) {
+        this.interestType = interestType;
+        this.name = name;
+    }
+
+    public static Interest create(InterestType interestType, String name) {
+        return new Interest(interestType, name);
+    }
+
+
+}
