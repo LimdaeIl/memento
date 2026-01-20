6cab49f9a6d40d1201508e72754429a7f83138d8
LimdaeIl <optional94@naver.com>
2026-01-20 23:15:43 +0900

    ✨#17: 회원 에러 코드와 예외 처리 구현
    ExceptionHandler에서 전역 처리할 수 있도록 공통 예외를 extends 받고, 예외 코드는 implements 합니다.

---
+++ src/main/java/com/natural/memento/user/domain/exception/AuthErrorCode.java
@@ -0,0 +1,26 @@
+package com.natural.memento.user.domain.exception;
+
+import com.natural.memento.commons.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+public enum AuthErrorCode implements ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "인증/인가 : 가입된 회원이 아닙니다. 회원 ID: %s");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus status() {
+        return status;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
