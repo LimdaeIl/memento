ee5c67e1b3abe3880fb751f63412b5940a1f72d3
LimdaeIl <optional94@naver.com>
2026-01-21 11:46:55 +0900

    ✨#21: 회원 엔티티 구현

---
+++ src/main/java/com/natural/memento/user/domain/entity/User.java
@@ -0,0 +1,49 @@
+package com.natural.memento.user.domain.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "v1_users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 512)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, length = 20)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", nullable = false, length = 20)
+    private UserRole role;
+
+    @Embedded
+    private UserLocation location;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserInterest> interests = new ArrayList<>();
+
+}
---
+++ src/main/java/com/natural/memento/user/domain/entity/UserLocation.java
@@ -0,0 +1,30 @@
+package com.natural.memento.user.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class UserLocation {
+
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Column(name = "street", length = 100)
+    private String street;
+
+    @Column(name = "zipcode", length = 100)
+    private String zipcode;
+
+    @Column(name = "country", length = 100)
+    private String country;
+
+}
